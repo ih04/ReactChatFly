20659f1559cf8241ce8d4387bdb994e969379e67
ihor bilenko bilenko <89473652+ih04@users.noreply.github.com>
2025-04-23 17:34:40 +0200

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server:{
     proxy:{
       '/socket.io':{
-        target: 'http://localhost:4000',
+        target: process.env.VITE_WS_URL || 'http://localhost:4000',
         ws:true
       }
     }
